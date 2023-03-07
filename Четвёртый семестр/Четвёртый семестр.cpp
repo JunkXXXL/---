12dbeb1231c627576c00591c9946eaeb369aa510
Андрей Тарасенко <Lsa328@yandex.ru>
2023-03-07 11:38:29 +0700

    add merge and separate (dont work)

--- Четвёртый семестр/Четвёртый семестр.cpp
+++ Четвёртый семестр/Четвёртый семестр.cpp
@@ -13,21 +13,110 @@ bool createFileWithNumbers(const std::string& fileName, const int numbersCount, 
         return false;
     }
 
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_real_distribution<double> dist(0., maxNumberValue);
+    std::srand(std::time(nullptr));
     for (int i = 0; i < numbersCount; i++)
     {
-        fFile << dist(mt) << " ";
+        fFile << rand() % maxNumberValue << " ";
     }
 
     fFile.close();
     return true;
 }
 
+void Merge(const std::string& fileName, int p)
+{
+    std::ofstream Ffile;
+    Ffile.open(fileName);
+    std::ifstream* fls = new std::ifstream[2];
+    fls[0].open("Afile.txt");
+    fls[1].open("Bfile.txt");
+
+    if (!fls[0].is_open() || !fls[1].is_open() || !Ffile.is_open() || !fls[0] || !fls[1])
+    {
+        return;
+    }
+
+    short pointer = 0;
+    double Anumber, Bnumber;
+    fls[0] >> Anumber; fls[1] >> Bnumber;
+    while (fls[0] && fls[1])
+    {
+        if (Anumber < Bnumber)
+        {
+            Ffile << Anumber;
+            fls[0] >> Anumber;
+        }
+        else
+        {
+            Ffile << Bnumber;
+            fls[1] >> Bnumber;
+        }
+    }
+
+    while (fls[0])
+    {
+        fls[0] >> Anumber;
+        Ffile << Anumber;
+    }
+
+    while (fls[1])
+    {
+        fls[1] >> Bnumber;
+        Ffile << Bnumber;
+    }
+
+    fls[0].close(); fls[1].close(); Ffile.close();
+    delete[] fls;
+}
+
+bool Separate(const std::string& fileName, int p)
+{
+    std::ifstream Ffile;
+    Ffile.open(fileName);
+    std::ofstream* fls = new std::ofstream[2];
+    fls[0].open("Afile.txt");
+    fls[1].open("Bfile.txt");
+
+    if (!fls[0].is_open() || !fls[1].is_open() || !Ffile.is_open())
+    {
+        return false;
+    }
+
+    int pointer = 0;
+    double number;
+    while (Ffile)
+    {
+        for (int i = 0; i < p; i++)
+        {
+            Ffile >> number;  fls[pointer % 2] << number;
+        }
+        pointer++;
+    }
+
+    if (!fls[0] || !fls[1])
+    {
+        fls[0].close(); fls[1].close(); Ffile.close();
+        delete[] fls;
+        Merge(fileName, p);
+        return true;
+    }
+    else {
+        fls[0].close(); fls[1].close(); Ffile.close();
+        delete[] fls;
+        return false;
+    }
+}
+
 void sortFile(const std::string& fileName)
 {
-
+    int p = 1;
+    while (!Separate(fileName, p))
+    {
+        Merge(fileName, p);
+        p = p * 2;
+        std::cout << p << " ";
+    }
+    
 }
 
 bool isFileContainsSortedArray(const std::string& fileName)
@@ -52,6 +141,7 @@ bool isFileContainsSortedArray(const std::string& fileName)
         }
         lastNumber = currentNumber;
     }
+    fFile.close();
     return true;
 }
 
@@ -73,7 +163,7 @@ int createAndSortFile(const std::string& fileName, const int numbersCount, const
 int main()
 {
     std::string fileName = "file.txt";
-    const int numbersCount = 1000000;
+    const int numbersCount = 100;
     const int maxNumberValue = 100000;
 
     for (int i = 0; i < 1; i++) {
